f96253d27c79c3bfcce7ba219e2662dfe2f9b96d
Thomas Sheehan <tsheehan@indeed.com>
2024-06-29 08:55:28 +0900

    complete exercise

--- src/features/notes/list/index.js
+++ src/features/notes/list/index.js
@@ -1,7 +1,13 @@
-export default function NotesList() {
-  const notes = [];
+import { useDispatch, useSelector } from "react-redux";
+import { removeNote, selectFilteredNotes } from "../notes.slice";
 
-  function handleDeleteButtonClick(id) {}
+export default function NotesList() {
+  const notes = useSelector(selectFilteredNotes);
+  const dispatch = useDispatch();
+
+  function handleDeleteButtonClick(id) {
+    dispatch(removeNote(id));
+  }
 
   const notesListItems = notes.map(({ id, note }) => (
     <li key={id}>
